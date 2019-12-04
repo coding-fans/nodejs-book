548fbb9153664cdc24c913f5bfb8c3112bb79565
huangxiaoyan <hxysnail@gmail.com>
2019-12-04 15:08:21 +0800

    mysql init commit

---
+++ src/database/mysql/demo/demo.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'to0r'
+})
+
+con.connect(async function(err) {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('connect success')
+
+  con.query('CREATE DATABASE user', function(err, result) {
+    if (err) {
+      return console.log('create database err', err)
+    }
+
+    console.log('create database user', result)
+  })
+
+})
